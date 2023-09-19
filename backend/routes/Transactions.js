9ee268d06d8198d4ffcfcf8727faf6915bae6ef5
PoorviS <pshrivastava@zmags.com>
2023-09-20 00:05:55 +0530

    Added error handling to handleTransactions & fetchAllTransactions

--- backend/routes/Transactions.js
+++ backend/routes/Transactions.js
@@ -3,59 +3,82 @@ const base64Url = require('base64-url');
 
 module.exports = {
     async handleTransactions(req, res) {
-        const connection = await pool.getConnection()
+        let connection;
         try {
-            const { walletId } = req.params;
+            connection = await pool.getConnection();
+
+            // Validate parameters and request body
+            const walletId = req.params.walletId;
             const { transactionId, amount, description, type } = req.body;
 
-            const [row, fields] = await connection.query(`SELECT * FROM Wallet WHERE id=?`, [walletId]);
+            if (!walletId || isNaN(walletId) || !amount || isNaN(amount) || !type || !['DEBIT', 'CREDIT'].includes(type)) {
+                return res.status(400).json({ error: 'Invalid input parameters' });
+            }
 
-            const result = row ? row[0] : {}
-            if (!result.id) {
+            const [rows] = await connection.query('SELECT * FROM Wallet WHERE id=?', [walletId]);
+            const wallet = rows.length > 0 ? rows[0] : null;
+
+            if (!wallet) {
                 return res.status(404).json({ error: 'Wallet not found' });
             }
 
-            let newBalance = parseFloat(result.balance);
+            let newBalance = parseFloat(wallet.balance);
+            const transactionAmount = parseFloat(amount);
 
-            if (type === "DEBIT") {
-                newBalance -= parseFloat(amount)
+            if (type === 'DEBIT') {
+                if (newBalance < transactionAmount) {
+                    return res.status(400).json({ error: 'Insufficient balance for this transaction!' });
+                }
+                newBalance -= transactionAmount;
             } else {
-                newBalance += parseFloat(amount)
+                newBalance += transactionAmount;
             }
 
-            await connection.query('UPDATE Wallet SET balance = ? WHERE id = ?', [Number.parseFloat(newBalance).toFixed(4), result.id]);
+            await connection.query('UPDATE Wallet SET balance=? WHERE id=?', [newBalance.toFixed(4), wallet.id]);
 
             await connection.query(
                 'INSERT INTO Transactions (id, wallet_id, amount, description, type, balance_after_transaction) VALUES (?, ?, ?, ?, ?, ?)',
-                [transactionId, result.id, amount, description, type, Number.parseFloat(newBalance).toFixed(4)]
+                [transactionId, wallet.id, transactionAmount, description, type, newBalance.toFixed(4)]
             );
 
-            res.json({ balance: Number.parseFloat(newBalance).toFixed(4), transactionId, type });
+            res.json({ balance: newBalance.toFixed(4), transactionId, type });
 
         } catch (error) {
-            res.status(500).json({ error: 'Transaction failed. Error: ' + error });
+            console.error(`Transaction failed: ${error}`);
+            res.status(500).json({ error: 'Transaction failed' });
         } finally {
-            connection.release()
+            if (connection) {
+                connection.release();
+            }
         }
     },
-    
+
     async fetchAllTransactions(req, res) {
-        const connection = await pool.getConnection();
+        let connection;
         try {
-          const {walletId} = req.params;
+            connection = await pool.getConnection();
 
-          const [rows] = await connection.query(`SELECT * FROM Transactions WHERE wallet_id=${walletId} ORDER BY created_at DESC`);
-          
-          if (rows.length === 0) {
-            return res.status(404).json({ error: 'Wallet not found' });
-          }
+            // Validate walletId
+            const walletId = req.params.walletId;
+            if (!walletId || isNaN(walletId)) {
+                return res.status(400).json({ error: 'Invalid wallet ID' });
+            }
 
-          res.json(rows);
-          
+            const [rows] = await connection.query('SELECT * FROM Transactions WHERE wallet_id=? ORDER BY created_at DESC', [walletId]);
+
+            if (rows.length === 0) {
+                return res.status(404).json({ error: 'No transactions found for this wallet' });
+            }
+
+            res.json(rows);
+
         } catch (error) {
-          res.status(500).json({ error: 'Failed to fetch wallet details' });
+            console.error(`Failed to fetch transactions: ${error}`);
+            res.status(500).json({ error: 'Failed to fetch transactions' });
         } finally {
-          connection.release();
+            if (connection) {
+                connection.release();
+            }
         }
     },
 
@@ -64,37 +87,37 @@ module.exports = {
         try {
             connection = await pool.getConnection();
             if (!connection) throw new HTTPError("Failed to establish a database connection.");
-    
+
             const { walletId, page = 1, limit = 10, offset: currentOffset } = req.query;
             if (!walletId) throw new HTTPError('Wallet ID must be provided.');
-    
+
             const intPage = Number(page), intLimit = Number(limit);
             if (intPage < 1 || intLimit < 1 || intLimit > 1000) throw new HTTPError('Invalid page or limit.');
-    
+
             const userOffset = currentOffset ? JSON.parse(base64Url.decode(currentOffset)) : undefined;
             const offset = userOffset?.nextContinuationToken || (intPage - 1) * intLimit;
-    
+
             const [transactions] = await connection.query('SELECT * FROM Transactions WHERE wallet_id = ? ORDER BY created_at DESC LIMIT ? OFFSET ?', [walletId, intLimit + 1, offset]);
-    
+
             let nextContinuationToken = null;
-    
+
             if (transactions.length > intLimit) {
                 nextContinuationToken = offset + intLimit;
                 transactions.pop();
             }
-    
+
             const prevContinuationToken = (offset - intLimit >= 0) ? offset - intLimit : null;
-    
+
             const baseLink = `http://localhost:8000/transactions?walletId=${walletId}&limit=${intLimit}`;
             const selfLink = currentOffset ? `${baseLink}&offset=${currentOffset}` : `${baseLink}&page=${intPage}`;
             const nextLink = nextContinuationToken !== null ? `${baseLink}&offset=${base64Url.encode(JSON.stringify({ nextContinuationToken }))}` : undefined;
             const prevLink = prevContinuationToken !== null ? `${baseLink}&offset=${base64Url.encode(JSON.stringify({ nextContinuationToken: prevContinuationToken }))}` : undefined;
-    
+
             res.json({
                 links: { self: selfLink, next: nextLink, prev: prevLink },
                 data: { transactions }
             });
-    
+
         } catch (err) {
             console.error(err);
             const status = err instanceof HTTPError ? 400 : 500;
@@ -103,6 +126,6 @@ module.exports = {
             if (connection) connection.release();
         }
     }
-    
+
 
 }
